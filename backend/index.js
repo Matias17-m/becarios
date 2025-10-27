4970fd98acbefdb3974c149670ed0267a090d818
Matias17-m <matiasicloud7@gmail.com>
2025-10-27 18:00:32 -0300

    Proyecto becarios funcionando

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+
+const db = new sqlite3.Database('../becal_sqlite.db');
+
+app.get('/becarios', (req, res) => {
+  db.all('SELECT * FROM BECAL_IMPORTADO', [], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor backend corriendo en http://localhost:${PORT}`);
+});
