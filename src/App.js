007439ed65466915c2a9cc639dda43f95ef212f7
Matias17-m <matiasicloud7@gmail.com>
2025-10-31 10:59:31 -0300

    Actualizando todos los archivos del frontend

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Becarios from './Becarios';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Consulta de Becarios</h1>
+      <Becarios />
+    </div>
+  );
+}
+
+export default App;
